59c70fde76fbf9642c5edcff214979faef02b9e0
Younghwan Cha <chayounghwan0615@gmail.com>
2022-10-11 00:41:14 +0900

    cvlib test

--- cvlibTest.py
+++ cvlibTest.py
@@ -1,18 +1,24 @@
 import cvlib as cv
-from cvlib.object_detection import draw_bbox
 import cv2
+import sys
+import os
 
-#-- 이미지 경로
-obj_img = cv2.imread('.static//tesla.jpeg')
+img = cv2.imread(sys.argv[1])
 
-#-- detect 함수 불러오기
-bbox, label, conf = cv.detect_common_objects(obj_img)
+faces, confis = cv.detect_face(img)
 
-#-- 검출 객체 박스 처리
-if label:
-    out = draw_bbox(obj_img, bbox, label, conf, write_conf=True)
+print(faces)
+print(confis)
 
-cv2.imshow("cvlib_img_test", obj_img)
+for face, confi in zip(faces, confis):
+	startX, startY = face[0], face[1]
+	endX, endY = face[2], face[3]
+	textX, textY = endX + 5, startY
 
+	cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
+	cv2.putText(img, "face, {:.3f}".format(confi), (textX, textY), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+cv2.imshow("cvlibTest", img)
 cv2.waitKey()
+cv2.imwrite("./static/cvlibTestResult3.jpg", img)
 cv2.destroyAllWindows()
